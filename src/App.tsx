6b4056d94509b6e87812902163ee667b61f29388
JohanaDahan <74196888+JohanaDahan@users.noreply.github.com>
2022-03-10 10:34:40 +0100

    managers debut

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,8 @@ import { Services } from "./Services";
 import { World } from './world';
 import ProductComponent from './Product'
 import { transform } from "./utils";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-
-
-  
 
 function App() {
   const [services, setServices] = useState(new Services(""))
@@ -24,23 +22,27 @@ function App() {
   }, [])
 
   const [showManager, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
   
   return (
 
     <div className="App">
       <div className="header">
-        <div><img src={services.server + world.logo} /></div>
+        <img src={services.server + world.logo} />
         <div className="titre">
           {world.name}
         </div>
-        <ul className="listeHeader">
+        <div className="listeHeader">
           <li>{services.user}</li>
           <li>Score : {world.score}</li>
           <li>Money : <span dangerouslySetInnerHTML={{ __html: transform(world.money) }} /> $</li>
-        </ul>
+        </div>
       </div>
       <div className="main">
         <div> liste des boutons de menu </div>
+      
         <div className="product">
           <div> <ProductComponent prod={world.products.product[0]} services={services} /> </div>
           <div> <ProductComponent prod={world.products.product[1]} services={services} /> </div>
@@ -50,6 +52,9 @@ function App() {
           <div> 6 </div>
           </div>
           </div>
+          
+          <ul className="listbtn">
+          <button onClick={() => handleShow()}><i className="btnManagers"></i>Managers</button></ul>
           <div> {showManager &&
         < div className="modal">
         <div>
@@ -60,16 +65,17 @@ function App() {
             {world.managers.pallier.filter(manager => !manager.unlocked).map(
               manager => (
                 <div key={manager.idcible} className="managergrid">
-                  <div className="composantGrid" id="managerLogo">
+                  <div className="logoGrid" id="managerLogo">
                     <img alt="manager logo" className="round" src={services.server + manager.logo} />
                   </div>
-                  <div className="composantGrid" id="infosManagers">
+                  <div className="infosGrid" id="infosManagers">
                     <div> {manager.name} </div>
                     <div> {world.products.product[manager.idcible - 1].name}</div>
-                    <div className="composantGrid" id="managerSeuil"> {manager.seuil} </div>
+                    <div className="seuilGrid" id="managerSeuil"> {manager.seuil} </div>
                   </div>
-                  <div id="boutonEngager">
-                    <button disabled={world.money < manager.seuil}> Engager! </button>
+                  <div id="closebutton">
+                    <button disabled={world.money < manager.seuil}> Hire! </button>
+                    
                   </div>
                 </div>
               ))}
